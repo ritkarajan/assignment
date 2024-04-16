fcc2fa85201e7d17c031d27b0e2f52693b0b3fe7
ritkarajan <122623251+ritkarajan@users.noreply.github.com>
2024-04-16 03:12:43 -0700

    Add files via upload

---
+++ post.js
@@ -0,0 +1,11 @@
+const mongoose=require('mongoose');
+
+const schema=mongoose.Schema({
+  name:String,
+  location:String,
+   position :String,
+   salary:Number
+})
+
+const postModel=mongoose.model('post',schema);
+ module.exports=postModel;
